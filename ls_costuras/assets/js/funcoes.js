066f28ef83a41fe59901b33faa53699eaec92730
Rogério Soares <rgrsoares@yahoo.com.br>
2022-08-19 16:03:01 -0300

    Updated masks

--- ls_costuras/assets/js/funcoes.js
+++ ls_costuras/assets/js/funcoes.js
@@ -1,7 +1,7 @@
 function calcular() {
     var n1 = parseInt(document.getElementById("service_cost").value, 10);
     var n2 = parseInt(document.getElementById("material_price").value, 10);
-    document.getElementById("net_value").value = n1 - n2;
+    document.getElementById("net_value").value = k(n1 - n2);
 }
 
 /* Máscaras ER ///Begin */
@@ -30,19 +30,12 @@ window.onload = function () {
 /* Máscaras ER ///End */
 
 /* Mascara para dinheiro Início*/
-function formatarMoeda(value) {
-    var elemento = document.getElementById(value);
-    var valor = elemento.value;
-
-    valor = valor + "";
-    valor = parseInt(valor.replace(/[\D]+/g, ""));
-    valor = valor + "";
-    valor = valor.replace(/([0-9]{2})$/g, ",$1");
-
-    if (valor.length > 6) {
-        valor = valor.replace(/([0-9]{3}),([0-9]{2}$)/g, ".$1,$2");
-    }
-
-    elemento.value = valor;
+function k(i) {
+    var v = i.value.replace(/\D/g, "");
+    v = (v / 100).toFixed(2) + "";
+    v = v.replace(".", ",");
+    v = v.replace(/(\d)(\d{3})(\d{3}),/g, "$1.$2.$3,");
+    v = v.replace(/(\d)(\d{3}),/g, "$1.$2,");
+    i.value = v;
 }
 /* Mascara para dinheiro Fim */
